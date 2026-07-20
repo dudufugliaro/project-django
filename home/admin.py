18b667aec4a3df92eb24bf1c1404cbaf5922584b
Luiz Eduardo Fugliaro <luizeduardofugliaro@outlook.com>
2026-07-20 16:01:39 -0300

    feat: adiciona tag no admin

--- home/admin.py
+++ home/admin.py
@@ -3,8 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Mensagem
 from .models import Categoria
-@admin.register(Categoria)                                       # ← novo
+@admin.register(Categoria)                                       
 class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    search_fields = ("nome",)
+
+
+@admin.register(Tag)                                            
+class TagAdmin(admin.ModelAdmin):
     list_display = ("nome",)
     search_fields = ("nome",)
 
@@ -12,5 +18,6 @@ class CategoriaAdmin(admin.ModelAdmin):
 @admin.register(Mensagem)
 class MensagemAdmin(admin.ModelAdmin):
     list_display = ("titulo", "criada_em","categoria")
-    list_filter = ("categoria",)  
+    list_filter = ("categoria","tags")  
     search_fields = ("titulo", "conteudo")
+    filter_horizontal = ("tags",)
