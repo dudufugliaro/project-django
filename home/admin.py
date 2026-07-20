b9ba572f69a9ebe8d4e1a1de98117ce0e5055de3
Luiz Eduardo Fugliaro <luizeduardofugliaro@outlook.com>
2026-07-20 15:21:31 -0300

    feat: adiciona novo model categoria e registra no admin

--- home/admin.py
+++ home/admin.py
@@ -3,8 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Mensagem
 
+@admin.register(Categoria)                                       # ← novo
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    search_fields = ("nome",)
+
 
 @admin.register(Mensagem)
 class MensagemAdmin(admin.ModelAdmin):
-    list_display = ("titulo", "criada_em")
+    list_display = ("titulo", "criada_em","categoria")
+    list_filter = ("categoria",)  
     search_fields = ("titulo", "conteudo")
--- home/models.py
+++ home/models.py
@@ -3,6 +3,15 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
+class Categoria(models.Model):                                  # ← novo model
+    nome = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ["nome"]
+
+    def __str__(self):
+        return self.nome
+
 
 class Mensagem(models.Model):
     titulo = models.CharField(max_length=120)
