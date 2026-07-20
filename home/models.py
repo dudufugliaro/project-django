d261ee0fd458399202c00c1dd8b17ab7e9dee75f
Luiz Eduardo Fugliaro <luizeduardofugliaro@outlook.com>
2026-07-20 14:54:02 -0300

    feat: adiciona campo autor no modelo Mensagem

--- home/models.py
+++ home/models.py
@@ -8,6 +8,7 @@ class Mensagem(models.Model):
     titulo = models.CharField(max_length=120)
     conteudo = models.TextField()
     criada_em = models.DateTimeField(auto_now_add=True)
+    autor = models.CharField(max_length=80, default="Anônimo")   
 
     class Meta:
         ordering = ["-criada_em"]
