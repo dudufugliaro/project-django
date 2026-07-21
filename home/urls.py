7eb345285a6e4af4a88b6fc7d41173c4b52f43dd
Luiz Eduardo Fugliaro <luizeduardofugliaro@outlook.com>
2026-07-20 21:52:24 -0300

    feat: adiciona url para nova editar mensagem

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("sobre/", views.sobre, name="sobre"),
     path("nova/", views.nova_mensagem, name="nova_mensagem"),
+    path("mensagens/<int:id>/editar/", views.editar_mensagem, name="editar_mensagem"),
 ]
