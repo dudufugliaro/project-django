5613d1e43f37e8243b85d0ccc30b4bc97c720fa5
Luiz Eduardo Fugliaro <luizeduardofugliaro@outlook.com>
2026-07-20 21:51:10 -0300

    feat: adiciona logica para aplicar tag e editar mensagem

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils.text import slugify
+
 
 # Create your views here.
 from .forms import MensagemForm
@@ -11,6 +15,15 @@ def index(request):
 
 def sobre(request):                                  
     return render(request, "home/sobre.html")
+
+def _aplicar_tags(mensagem, tags_texto):
+    """Substitui as tags da mensagem pelas que vieram do formulário."""
+    mensagem.tags.clear()
+    for pedaco in tags_texto.split(","):
+        nome = slugify(pedaco)
+        if nome:
+            tag, _ = Tag.objects.get_or_create(nome=nome)
+            mensagem.tags.add(tag)
 
 def nova_mensagem(request):
     if request.method == "POST":
@@ -33,3 +46,19 @@ def nova_mensagem(request):
         form = MensagemForm()
 
     return render(request, "home/nova.html", {"form": form})
+
+def editar_mensagem(request, id):
+    mensagem = get_object_or_404(Mensagem, id=id)
+
+    if request.method == "POST":
+        form = MensagemForm(request.POST, instance=mensagem)
+        if form.is_valid():
+            mensagem = form.save()
+            _aplicar_tags(mensagem, form.cleaned_data["tags"])
+            messages.success(request, "Mensagem atualizada com sucesso!")
+            return redirect("index")
+    else:
+        tags_atuais = ", ".join(tag.nome for tag in mensagem.tags.all())
+        form = MensagemForm(instance=mensagem, initial={"tags": tags_atuais})
+
+    return render(request, "home/editar.html", {"form": form, "mensagem": mensagem})
